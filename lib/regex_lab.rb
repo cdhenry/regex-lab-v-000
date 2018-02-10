cb38b077c7c9066f428a8d1133ab601c50e77aeb
Chris Henry <cdhenry@gmail.com>
2018-02-10 11:36:53 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,8 @@
 def starts_with_a_vowel?(word)
-  word.scan(/\b[AEIOUaeiou][a-z]\b/)
+  if word.scan(/\b[AEIOUaeiou][a-z]*\b/) != nil
+    return true
+  else
+    return false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
